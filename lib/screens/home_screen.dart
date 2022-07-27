820ac1a4f10eee38b192744a700ed09e1c671ee9
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-07-26 22:17:26 -0300

    Criacao do titulo para visualizar as passagens e O botao de ver tudo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -39,33 +39,52 @@ class HomeScreen extends StatelessWidget {
                       height: 50,
                       width: 50,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: const DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: AssetImage(
-                              "assets/images/img_1.jpg"
-                          )
-                        )
-                      ),
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.fitHeight,
+                              image: AssetImage("assets/images/img_1.jpg"))),
                     )
                   ],
                 ),
                 const Gap(25),
                 Container(
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color(0xFFF4F6FD)
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFFF4F6FD)),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
                   child: Row(
                     children: [
-                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
+                      const Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205),
+                      ),
                       Text(
                         "Search",
                         style: Styles.headLineStyle4,
                       )
                     ],
                   ),
+                ),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Upcoming Flights",
+                      style: Styles.headLineStyle2,
+                    ),
+                    InkWell(
+                      onTap: (){
+                        print("You are tapped");
+                      },
+                      child: Text(
+                        "View all",
+                        style: Styles.textStyle
+                            .copyWith(color: Styles.primaryColor),
+                      ),
+                    )
+                  ],
                 )
               ],
             ),
